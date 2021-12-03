53f4187e8792fdfadfb8a49fafbe16f2ede480d3
Eric Chin <ericychin94@gmail.com>
2021-12-03 03:09:59 -0800

    Added code for some extra notes

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+        </div>
+    )
+};
+
 const Hello = (props) => {
     return (
         <div>
@@ -15,11 +23,12 @@ const App = () => {
     const age = 10;
 
     return (
-        <div>
+        <>
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10} />
             <Hello name={name} age={age} />
-        </div>
+            <Footer />
+        </>
     )
 };
 
