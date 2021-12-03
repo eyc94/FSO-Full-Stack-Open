05c59865e6043abc3557ecb86ce3ab066d9ce28f
Eric Chin <ericychin94@gmail.com>
2021-12-03 15:48:30 -0800

    Added the Content component

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -8,8 +8,20 @@ const Header = (props) => {
     )
 };
 
-const Content = () => {
-
+const Content = (props) => {
+    return (
+        <>
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+            <p>
+                {props.part2} {props.exercises2}
+            </p>
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+        </>
+    )
 };
 
 const Total = () => {
@@ -28,15 +40,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
             <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
         </div>
     )
