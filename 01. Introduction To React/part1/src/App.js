b1fe760a457365e1b04244152cbaaac4d12ba466
Eric Chin <ericychin94@gmail.com>
2021-12-03 23:32:47 -0800

    Added helper function function

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Hello = (props) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear();
+        return yearNow - props.age;
+    };
+
     return (
         <div>
             <p>
                 Hello {props.name}, you are {props.age} years old
             </p>
+            <p>So you were probably born in {bornYear()}</p>
         </div>
     )
 };
@@ -19,7 +25,6 @@ const App = () => {
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10} />
             <Hello name={name} age={age} />
-            <Footer />
         </div>
     )
 };
