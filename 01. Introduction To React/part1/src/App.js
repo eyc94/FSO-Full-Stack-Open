442d9fac5bd9a318cf1d394b7bdec096e3f37727
Eric Chin <ericychin94@gmail.com>
2021-12-04 03:00:43 -0800

    Added code for page re-rendering and auto incrementing counter

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
 
-const Hello = ({ name, age }) => {
-    const bornYear = () => new Date().getFullYear() - age;
-
+const App = (props) => {
+    const { counter } = props;
     return (
-        <div>
-            <p>
-                Hello {name}, you are {age} years old
-            </p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
+        <div>{counter}</div>
     )
 };
 
-const App = () => {
-    const name = "Peter";
-    const age = 10;
+// Code before "Page re-rendering" section of part 1.
+// const Hello = ({ name, age }) => {
+//     const bornYear = () => new Date().getFullYear() - age;
 
-    return (
-        <div>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 10} />
-            <Hello name={name} age={age} />
-        </div>
-    )
-};
+//     return (
+//         <div>
+//             <p>
+//                 Hello {name}, you are {age} years old
+//             </p>
+//             <p>So you were probably born in {bornYear()}</p>
+//         </div>
+//     )
+// };
+
+// const App = () => {
+//     const name = "Peter";
+//     const age = 10;
+
+//     return (
+//         <div>
+//             <h1>Greetings</h1>
+//             <Hello name="Maya" age={26 + 10} />
+//             <Hello name={name} age={age} />
+//         </div>
+//     )
+// };
 
 export default App;
