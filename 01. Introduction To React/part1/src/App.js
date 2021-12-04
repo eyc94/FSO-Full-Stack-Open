2a1d7f69be474a81a257700e6d08ecf4917fc625
Eric Chin <ericychin94@gmail.com>
2021-12-03 20:52:36 -0800

    Started new example for part1c

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-const Footer = () => {
-    return (
-        <div>
-            greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-        </div>
-    )
-};
-
 const Hello = (props) => {
     return (
         <div>
@@ -23,12 +15,12 @@ const App = () => {
     const age = 10;
 
     return (
-        <>
+        <div>
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10} />
             <Hello name={name} age={age} />
             <Footer />
-        </>
+        </div>
     )
 };
 
