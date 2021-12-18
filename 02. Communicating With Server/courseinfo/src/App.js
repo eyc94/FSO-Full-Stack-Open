2adb35549844461e862123b16d7d1bf3aa3885a5
Eric Chin <ericychin94@gmail.com>
2021-12-18 10:13:43 -0800

    Created a Course component and used the map method to handle case of dynamic array

--- 02. Communicating With Server/courseinfo/src/App.js
+++ 02. Communicating With Server/courseinfo/src/App.js
@@ -11,9 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
-            <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
-            <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />
+            {props.course.parts.map(part =>
+                <Part key={part.id} part={part.name} exercise={part.exercises} />
+            )}
         </>
     )
 };
@@ -36,32 +36,40 @@ const Total = (props) => {
     )
 };
 
+const Course = (props) => {
+    return (
+        <div>
+            <Header course={props.course} />
+            <Content course={props.course} />
+            {/* <Total course={props.course} /> */}
+        </div>
+    )
+};
+
 const App = () => {
     const course = {
+        id: 1,
         name: 'Half Stack application development',
         parts: [
             {
                 name: 'Fundamentals of React',
-                exercises: 10
+                exercises: 10,
+                id: 1
             },
             {
                 name: 'Using props to pass data',
-                exercises: 7
+                exercises: 7,
+                id: 2
             },
             {
                 name: 'State of a component',
-                exercises: 14
+                exercises: 14,
+                id: 3
             }
         ]
     };
 
-    return (
-        <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </div>
-    )
+    return <Course course={course} />
 }
 
 export default App;
