78a59eddf6918841ad5ce66186710f971aba24c9
Eric Chin <ericychin94@gmail.com>
2021-12-22 18:27:17 -0800

    Added new note adding functionality

--- 02. Communicating With Server/notes-frontend-part2/src/App.js
+++ 02. Communicating With Server/notes-frontend-part2/src/App.js
@@ -3,6 +3,24 @@ import Note from './components/Note';
 
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState('');
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notes.length + 1
+        };
+        setNotes(notes.concat(noteObject));
+        setNewNote('');
+    };
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
 
     return (
         <div>
@@ -12,6 +30,13 @@ const App = (props) => {
                     <Note key={note.id} note={note} />
                 )}
             </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 };
