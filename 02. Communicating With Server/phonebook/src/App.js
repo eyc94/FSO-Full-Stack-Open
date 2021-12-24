475ed7c6827f23ddae75bb7f0063bc2462bdd3b6
Eric Chin <ericychin94@gmail.com>
2021-12-23 21:50:21 -0800

    Allowed user to add phone number as well

--- 02. Communicating With Server/phonebook/src/App.js
+++ 02. Communicating With Server/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '123-342-8382' }
     ]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
     const addPerson = (event) => {
         event.preventDefault();
         const personObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         };
 
         if (persons.filter(person => person.name.toLowerCase() === newName.toLowerCase()).length > 0) {
@@ -19,27 +21,28 @@ const App = () => {
             setPersons(persons.concat(personObject));
         }
         setNewName('');
+        setNewNumber('');
     };
 
     const handlePersonChange = (event) => {
-        console.log(event.target.value);
         setNewName(event.target.value);
     };
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handlePersonChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
+                <div>name: <input value={newName} onChange={handlePersonChange} /></div>
+                <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+                <div><button type="submit">add</button></div>
             </form>
             <h2>Numbers</h2>
             {persons.map(person =>
-                <div key={person.name}>{person.name}</div>
+                <div key={person.name}>{person.name} {person.number}</div>
             )}
         </div>
     )
