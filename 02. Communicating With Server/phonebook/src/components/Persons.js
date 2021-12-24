89f2e1b5e4a9406d712ad72deeafe4e77efa10e3
Eric Chin <ericychin94@gmail.com>
2021-12-24 13:18:38 -0800

    Added Person component to handle single person rendering

--- 02. Communicating With Server/phonebook/src/components/Persons.js
+++ 02. Communicating With Server/phonebook/src/components/Persons.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Person from './Person';
 
 const Persons = (props) => {
     return (
         <>
             {props.persons.map(person =>
-                <div key={person.name}>{person.name} {person.number}</div>
+                <Person key={person.name} name={person.name} number={person.number} />
             )}
         </>
     )
