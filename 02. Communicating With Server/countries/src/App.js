ef6ad0b7969319931030646d27c7f9323b1c491d
Eric Chin <ericychin94@gmail.com>
2021-12-27 15:58:18 -0800

    Added countries state with fetched countries from all endpoint

--- 02. Communicating With Server/countries/src/App.js
+++ 02. Communicating With Server/countries/src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
+    const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         axios
             .get('https://restcountries.com/v3.1/all')
             .then(response => {
-                console.log(response);
+                setCountries(response.data);
             });
     }, []);
 
