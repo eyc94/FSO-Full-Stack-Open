615d2a64fb1841ffbd5c306b4c8e6bef3c9692b6
Eric Chin <ericychin94@gmail.com>
2021-12-27 14:58:41 -0800

    Removed failed promise and retrieved notes

--- 02. Communicating With Server/notes-frontend-part2/src/index.js
+++ 02. Communicating With Server/notes-frontend-part2/src/index.js
@@ -5,8 +5,10 @@ import axios from 'axios';
 const promise = axios.get('http://localhost:3001/notes');
 console.log(promise);
 
-const promise2 = axios.get('http://localhost:3001/foobar');
-console.log(promise2);
+promise.then(response => {
+    const notes = response.data;
+    console.log(notes);
+});
 
 const notes = [
     {
