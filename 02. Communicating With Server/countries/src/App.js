282e51394816d825ee977a2f80f4a3cca7842196
Eric Chin <ericychin94@gmail.com>
2021-12-27 15:57:34 -0800

    Set useEffect

--- 02. Communicating With Server/countries/src/App.js
+++ 02. Communicating With Server/countries/src/App.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(response => {
+                console.log(response);
+            });
+    }, []);
+
     return (
         <div>
             <div>find countries <input /></div>
