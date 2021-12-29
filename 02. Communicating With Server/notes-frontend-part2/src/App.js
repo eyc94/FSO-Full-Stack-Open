900b1355405d5dd18103ecd7c4226eb3a929ccf5
Eric Chin <ericychin94@gmail.com>
2021-12-29 11:10:32 -0800

    Code on changing importance of note

--- 02. Communicating With Server/notes-frontend-part2/src/App.js
+++ 02. Communicating With Server/notes-frontend-part2/src/App.js
@@ -34,6 +34,18 @@ const App = () => {
         setNewNote(event.target.value);
     };
 
+    const toggleImportanceOf = (id) => {
+        const url = `http://localhost:3001/notes/${id}`;
+        const note = notes.find(n => n.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        axios
+            .put(url, changedNote)
+            .then(response => {
+                setNotes(notes.map(note => note.id !== id ? note : response.data));
+            });
+    };
+
     const notesToShow = showAll
         ? notes
         : notes.filter(note => note.important);
@@ -48,7 +60,11 @@ const App = () => {
             </div>
             <ul>
                 {notesToShow.map(note =>
-                    <Note key={note.id} note={note} />
+                    <Note
+                        key={note.id}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
                 )}
             </ul>
             <form onSubmit={addNote}>
