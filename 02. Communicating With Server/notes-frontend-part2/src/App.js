bcec871e5d5254f28f4c1182b56e328bdf25cdb8
Eric Chin <ericychin94@gmail.com>
2021-12-29 12:00:32 -0800

    Separated service module for axios

--- 02. Communicating With Server/notes-frontend-part2/src/App.js
+++ 02. Communicating With Server/notes-frontend-part2/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
     const [notes, setNotes] = useState([]);
@@ -8,8 +9,8 @@ const App = () => {
     const [showAll, setShowAll] = useState(true);
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/notes')
+        noteService
+            .getAll()
             .then(response => {
                 setNotes(response.data);
             });
@@ -22,8 +23,8 @@ const App = () => {
             date: new Date().toISOString(),
             important: Math.random() < 0.5,
         };
-        axios
-            .post('http://localhost:3001/notes', noteObject)
+        noteService
+            .create(noteObject)
             .then(response => {
                 setNotes(notes.concat(response.data));
                 setNewNote('');
@@ -35,12 +36,11 @@ const App = () => {
     };
 
     const toggleImportanceOf = (id) => {
-        const url = `http://localhost:3001/notes/${id}`;
         const note = notes.find(n => n.id === id);
         const changedNote = { ...note, important: !note.important };
 
-        axios
-            .put(url, changedNote)
+        noteService
+            .update(id, changedNote)
             .then(response => {
                 setNotes(notes.map(note => note.id !== id ? note : response.data));
             });
