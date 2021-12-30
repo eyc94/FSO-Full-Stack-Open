e2773c5bee16799749719f6589d3256a1d8d1f1f
Eric Chin <ericychin94@gmail.com>
2021-12-29 16:21:48 -0800

    Created del function in services and used in App and passed down to child components

--- 02. Communicating With Server/phonebook/src/components/Person.js
+++ 02. Communicating With Server/phonebook/src/components/Person.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 const Person = (props) => {
-    const deleteHandler = () => {
-        console.log('hi', props.identifier);
-    };
-
     return (
         <div key={props.name}>
             {props.name} {props.number}
-            <button onClick={deleteHandler}>Delete</button>
+            <button onClick={props.delPerson}>Delete</button>
         </div>
     )
 };
