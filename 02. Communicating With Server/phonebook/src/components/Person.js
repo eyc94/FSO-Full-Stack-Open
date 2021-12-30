a7b6b439fbd4ad48ea548bd0bad94d2032b572d5
Eric Chin <ericychin94@gmail.com>
2021-12-29 16:13:35 -0800

    Added button template for each person

--- 02. Communicating With Server/phonebook/src/components/Person.js
+++ 02. Communicating With Server/phonebook/src/components/Person.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Person = (props) => {
+    const deleteHandler = () => {
+        console.log('hi', props.identifier);
+    };
+
     return (
-        <div key={props.name}>{props.name} {props.number}</div>
+        <div key={props.name}>
+            {props.name} {props.number}
+            <button onClick={deleteHandler}>Delete</button>
+        </div>
     )
 };
 
--- 02. Communicating With Server/phonebook/src/components/Persons.js
+++ 02. Communicating With Server/phonebook/src/components/Persons.js
@@ -5,7 +5,7 @@ const Persons = (props) => {
     return (
         <>
             {props.persons.map(person =>
-                <Person key={person.name} name={person.name} number={person.number} />
+                <Person identifier={person.id} key={person.name} name={person.name} number={person.number} />
             )}
         </>
     )
