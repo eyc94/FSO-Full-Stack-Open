e4feea7154f8d256a857c4424e43906eebf3dbf0
Eric Chin <ericychin94@gmail.com>
2022-01-01 15:56:20 -0800

    Added Footer to application

--- 02. Communicating With Server/notes-frontend-part2/src/App.js
+++ 02. Communicating With Server/notes-frontend-part2/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
 import Notification from './components/Notification';
+import Footer from './components/Footer';
 import noteService from './services/notes';
 
 const App = () => {
@@ -85,6 +86,7 @@ const App = () => {
                 />
                 <button type="submit">save</button>
             </form>
+            <Footer />
         </div>
     )
 };
