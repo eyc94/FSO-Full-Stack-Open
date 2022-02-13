a2247ae8a593f76b891f54eaa28ba1bd5d820416
Eric Chin <ericychin94@gmail.com>
2022-02-13 04:25:31 -0800

    Added ability to save logged in user detail to local storage

--- 05. Testing React Apps/bloglist-frontend/src/App.js
+++ 05. Testing React Apps/bloglist-frontend/src/App.js
@@ -22,6 +22,10 @@ const App = () => {
             const user = await loginService.login({
                 username, password
             });
+
+            window.localStorage.setItem(
+                'loggedBlogappUser', JSON.stringify(user)
+            );
             setUser(user);
             setUsername('');
             setPassword('');
