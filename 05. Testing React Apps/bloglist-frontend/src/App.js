004575506637b9511a68517f1a3a11108348b44e
Eric Chin <ericychin94@gmail.com>
2022-02-13 05:13:37 -0800

    Reorganized forms and added ability to create blogs

--- 05. Testing React Apps/bloglist-frontend/src/App.js
+++ 05. Testing React Apps/bloglist-frontend/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [user, setUser] = useState(null);
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [url, setUrl] = useState('');
 
     useEffect(() => {
         blogService.getAll().then(blogs => {
@@ -20,8 +23,26 @@ const App = () => {
         if (loggedUserJSON) {
             const user = JSON.parse(loggedUserJSON);
             setUser(user);
+            blogService.setToken(user.token);
         }
     }, []);
+
+    const addBlog = (event) => {
+        event.preventDefault();
+        const blogObject = {
+            title: title,
+            author: author,
+            url: url
+        };
+        blogService
+            .create(blogObject)
+            .then(returnedBlog => {
+                setBlogs(blogs.concat(returnedBlog));
+                setTitle('');
+                setAuthor('');
+                setUrl('');
+            });
+    };
 
     const handleLogin = async (event) => {
         event.preventDefault();
@@ -34,6 +55,7 @@ const App = () => {
             window.localStorage.setItem(
                 'loggedBlogappUser', JSON.stringify(user)
             );
+            blogService.setToken(user.token);
             setUser(user);
             setUsername('');
             setPassword('');
@@ -42,51 +64,86 @@ const App = () => {
         }
     };
 
+    const loginForm = () => (
+        <div>
+            <h2>Log In To The Application</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input
+                        type="text"
+                        value={username}
+                        name="Username"
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                </div>
+                <div>
+                    password
+                    <input
+                        type="password"
+                        value={password}
+                        name="Password"
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+
+    const blogForm = () => (
+        <form onSubmit={addBlog}>
+            <div>
+                title:
+                <input
+                    type="text"
+                    value={title}
+                    name="Title"
+                    onChange={({ target }) => setTitle(target.value)}
+                />
+            </div>
+            <div>
+                author:
+                <input
+                    type="text"
+                    value={author}
+                    name="Author"
+                    onChange={({ target }) => setAuthor(target.value)}
+                />
+            </div>
+            <div>
+                url:
+                <input
+                    type="text"
+                    value={url}
+                    name="Url"
+                    onChange={({ target }) => setUrl(target.value)}
+                />
+            </div>
+            <button type="submit">Create</button>
+        </form>
+    );
+
     const userLogout = () => {
         window.localStorage.removeItem('loggedBlogappUser');
         setUser(null);
     };
 
-    if (user === null) {
-        return (
-            <div>
-                <h2>Log In To The Application</h2>
-                <form onSubmit={handleLogin}>
-                    <div>
-                        username
-                        <input
-                            type="text"
-                            value={username}
-                            name="Username"
-                            onChange={({ target }) => setUsername(target.value)}
-                        />
-                    </div>
-                    <div>
-                        password
-                        <input
-                            type="password"
-                            value={password}
-                            name="Password"
-                            onChange={({ target }) => setPassword(target.value)}
-                        />
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        );
-    }
-
     return (
         <div>
-            <h2>Blogs</h2>
-            <div>
-                <p>{user.name} logged in
-                    <button onClick={userLogout}>Logout</button>
-                </p>
-            </div>
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} />
-            )}
+            {user === null ?
+                loginForm() :
+                <div>
+                    <h2>Blogs</h2>
+                    <p>{user.name} logged in
+                        <button onClick={userLogout}>Logout</button>
+                    </p>
+                    {blogForm()}
+                    {blogs.map(blog =>
+                        <Blog key={blog.id} blog={blog} />
+                    )}
+                </div>
+            }
         </div>
     );
 };
