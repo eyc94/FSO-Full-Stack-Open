23bd46de2cf6419b55c54493f9563992328e0fdc
Eric Chin <ericychin94@gmail.com>
2022-02-23 21:29:14 -0800

    Added state to Blog component to handle visibility of details

--- 05. Testing React Apps/bloglist-frontend/src/components/Blog.js
+++ 05. Testing React Apps/bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 const Blog = ({ blog }) => {
+    const [showDetails, setShowDetails] = useState(false);
+
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -7,9 +11,28 @@ const Blog = ({ blog }) => {
         marginBottom: 5
     };
 
+    const viewHandler = () => {
+        setShowDetails(!showDetails);
+    };
+
+    const hideWhenShown = { display: showDetails ? 'none' : '' };
+    const showWhenShown = { display: showDetails ? '' : 'none' };
+
     return (
-        <div style={blogStyle}>
-            {blog.title} {blog.author}
+        <div>
+            <div style={hideWhenShown}>
+                <div style={blogStyle}>
+                    {blog.title} {blog.author} <button onClick={viewHandler}>view</button>
+                </div>
+            </div>
+            <div style={showWhenShown}>
+                <div style={blogStyle}>
+                    <div>{blog.title} <button onClick={viewHandler}>hide</button></div>
+                    <div>{blog.url}</div>
+                    <div>likes {blog.likes} <button>like</button></div>
+                    <div>{blog.author}</div>
+                </div>
+            </div>
         </div>
     );
 };
