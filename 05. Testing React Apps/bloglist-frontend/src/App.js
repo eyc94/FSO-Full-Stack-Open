0745725f2453e5546829925b1f9d2d05f864ad77
Eric Chin <ericychin94@gmail.com>
2022-02-25 14:14:57 -0800

    Passed blog object not id

--- 05. Testing React Apps/bloglist-frontend/src/App.js
+++ 05. Testing React Apps/bloglist-frontend/src/App.js
@@ -149,7 +149,7 @@ const App = () => {
                     </p>
                     {blogForm()}
                     {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
-                        <Blog key={blog.id} blog={blog} handleLike={() => handleLike(blog)} handleRemove={() => removeBlog(blog.id)} />
+                        <Blog key={blog.id} blog={blog} handleLike={() => handleLike(blog)} handleRemove={() => removeBlog(blog)} />
                     )}
                 </div>
             }
